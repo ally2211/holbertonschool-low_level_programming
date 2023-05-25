4a87c1e890c642956bcd742787bd88e75a9cf71e
root <6677@holbertonstudents.com>
2023-05-25 15:05:10 -0700

    undo

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d and is %d and is greater than 5\n", n, lastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 	}
 	return (0);
 }

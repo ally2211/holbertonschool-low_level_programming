0f12f90cdac133befd5c1a3919e0d5831f6785ed
root <6677@holbertonstudents.com>
2023-05-25 10:32:07 -0700

    commit spaces

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <time.h>
+
+/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
@@ -19,6 +21,5 @@ int main(void)
 	{
 		printf("%d is negative\n",n);
 	}
-	return 0;
+	return (0);
 }
-

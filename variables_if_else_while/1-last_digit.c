215c319a98742aca1757db8661ab5aa9b0bea826
root <6677@holbertonstudents.com>
2023-05-25 12:07:26 -0700

    commit last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,17 +13,20 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n == 0)
+	
+	int lastDigit = n % 10;
+
+	if (lastDigit == 0)
 	{
-		printf("Last digit of %d is 0 and is 0\n", n);
+		printf("Last digit of %d is 0 and is 0\n", n, lastDigit);
 	}
-	else if (n > 0)
+	else if (lastDigit < 0)
 	{
-		printf("Last digit of %d is less that 6 and not 0\n", n);
+		printf("Last digit of %d is less that 6 and not 0\n", n, lastDigit);
 	}
 	else
 	{
-		printf("Last digit of %d and is greater than 5\n", n);
+		printf("Last digit of %d and is greater than 5\n", n, lastDigit);
 	}
 	return (0);
 }

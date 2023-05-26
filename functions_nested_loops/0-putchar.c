3049b2feb28c4be098ebfe374096685799a4ff16
root <6677@holbertonstudents.com>
2023-05-25 23:53:17 -0700

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,22 @@
 #include <main.h>
+#include <stdio.h>
+
 /** 
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main() 
 {
-	putchar("_putchar\n");	
-	return (0);
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+
+    return 0;
 }

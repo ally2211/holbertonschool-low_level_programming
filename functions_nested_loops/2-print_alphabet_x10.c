e68227b8750c543cac0990fcc95f560941b04e88
root <6677@holbertonstudents.com>
2023-05-27 15:54:30 -0700

    10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - use function in header
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	counter = '1';
+
+	while (counter <= '10')
+	{
+		char letter = 'a';
+
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		counter++;
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,2 +1,2 @@
-void print_alphabet();
+void print_alphabet_x10();
 void _putchar();

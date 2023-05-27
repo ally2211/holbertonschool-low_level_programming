019af4958573573ee75d5810b3dcd75cacaea016
root <6677@holbertonstudents.com>
2023-05-27 12:39:20 -0700

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
+void print_alphabet(void);
+
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-void print_alphabet()
+void print_alphabet(void)
 {
 	char letter = 'a';
 	while (letter <= 'z')

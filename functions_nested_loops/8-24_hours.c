e645646e8dcc01851afe839c6c7d40a0b8e4f73d
root <6677@holbertonstudents.com>
2023-05-28 16:48:07 -0700

    minutes

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer- prints every minute of the day
+ *
+ * Return: always 0 (Success)
+ */
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			printf("02d:%02d\n", hour, minute);
+		}
+	}
+}

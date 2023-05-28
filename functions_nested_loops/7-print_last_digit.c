57ae3dd326bce8a5695b932ab125a21a642308af
root <6677@holbertonstudents.com>
2023-05-28 15:32:52 -0700

    7 last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_last_digit - returns the last digit
@@ -10,5 +11,6 @@ int print_last_digit(int n)
 {
 	int lastdigit = n % 10;
 
+	printf("%d", lastdigit);
 	return (lastdigit);
 }

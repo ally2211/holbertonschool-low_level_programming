e21c9fbafc42007b71de5cd8fa5488c71276ac81
root <6677@holbertonstudents.com>
2023-05-31 13:56:32 -0700

    0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include main.h
+
+/** 
+ * _isupper - is a function that checks uppercase
+ * @c: is argument of character to check
+ *
+ * Return:  0 if yes is upper case else return 1
+ */
+int _isupper(int c)
+{	
+	if (c > 0)
+		return (0);
+	else
+		return (1);
+}

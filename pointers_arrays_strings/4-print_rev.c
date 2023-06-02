c33c2538a4283f432bfce6ad4e587aa6f4ff41df
root <6677@holbertonstudents.com>
2023-06-02 14:07:17 -0700

    rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints reverse of a string, followed by a new line, to stdout
+ * @str: pointer passed
+ *
+ * Return: void
+ */
+void print_rev(char *str)
+{
+	while (*str != '\0')
+	{
+		str++;
+	}
+	str--;
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str--;
+	}
+	_putchar('\n');
+}

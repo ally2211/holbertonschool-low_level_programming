00a0047957919ae76974ab281c6141c0034c22a6
root <6677@holbertonstudents.com>
2023-06-19 10:13:05 -0700

    sqrroot

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+int squareRootHelper(int n, int start, int end);
+
+/**
+ * _sqrt_recursion - calculate square root using recursion
+ * @n: base
+ *
+ * Return: square root of n or -1 if none
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+	return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (squareRootHelper(n, 1, n / 2));
+	}
+}
+
+/**
+ * squareRootHelper - using start and n/2 as end iterate to find sqrroot
+ * base case: mid is also sqroot
+ * @n is number
+ * @start: is 1
+ * @end: is n/2
+ *
+ * Return:  sqrroot
+ */
+int squareRootHelper(int n, int start, int end)
+{
+	if (start <= end)
+	{
+		int mid = (start + end) / 2;
+		int square = mid * mid;
+
+		if (square == n)
+		{
+			return (mid);
+		}
+		else if (square < n)
+		{
+			return (squareRootHelper(n, mid + 1, end));
+		}
+		else
+		{
+			return (squareRootHelper(n, start, mid - 1));
+		}
+	}
+	else
+	{
+		return (-1);
+	}
+}

05e97ceb6a93eb7337ddfd37536f0d1bb51a39b9
root <6677@holbertonstudents.com>
2023-06-19 08:08:58 -0700

    factorial

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -11,7 +11,12 @@ int factorial(int n)
 	if (n > 0)
 		return (n * factorial(n - 1));
 	else if (n < 0)
-		return (n * factorial(n + 1));
+	{
+		if ((n + 1) == 0)
+			return (-1);
+		else
+			return (n * factorial(n + 1));
+	}
 	else
 		return (1);
 }

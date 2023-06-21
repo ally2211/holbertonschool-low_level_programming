ca785f131d79a045b0f5f5ea64c5904ec11afbde
root <6677@holbertonstudents.com>
2023-06-21 14:34:00 -0700

    add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - will print its name even if rename
+ * @argc: number of args
+ * @argv:  name of program is first always
+ *
+ * Return: 0 is Success
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 1;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+		{	
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}

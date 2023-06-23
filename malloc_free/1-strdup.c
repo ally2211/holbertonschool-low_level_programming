cfb291b04563253d67e431136c0ce0fee3861c3c
root <6677@holbertonstudents.com>
2023-06-23 03:08:32 -0700

    duplicated

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -15,7 +15,7 @@ char *_strdup(char *str)
 	char *newpointer = (char *)malloc(length * sizeof(char));
 	int i;
 
-	if (str == NULL || length == 0)
+	if (newpointer == NULL || length == 0)
 	{
 		return (NULL);
 	}

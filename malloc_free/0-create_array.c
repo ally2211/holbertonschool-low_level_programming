72be9be2c0ccb3f1fc37ffc1915d73950fa7ade8
root <6677@holbertonstudents.com>
2023-06-22 23:54:19 -0700

    deal with return

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	char *array = (char *)malloc(size * sizeof(char));
 	unsigned int i;
 
-	if (size == 0)
+	if (array == NULL)
 	{
 		return (NULL);
 	}

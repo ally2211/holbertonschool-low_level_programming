557e5654dc78177aa5f60c9ec88535dfe76bf672
root <6677@holbertonstudents.com>
2023-06-26 14:40:58 -0700

    macro

--- preprocessor/0-object_like_macro.h
+++ preprocessor/0-object_like_macro.h
@@ -1,7 +1,6 @@
-#ifndef 0-object_like_macro.H
-#define 0-object_like_macro.h
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
-#define PI 3.14159265359
-
-#endif
+#define PI  3.14159265359
+#endif 

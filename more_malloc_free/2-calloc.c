526b9124dc1def12ab9eaaad3dd445bc78cffe6d
root <6677@holbertonstudents.com>
2023-06-26 11:59:46 -0700

    void ptr

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -7,28 +7,33 @@
  * _calloc - function that allocates memory for an array
  * @nmemb: an array of nmemb elements
  * @size: size of bytes each
- * @n: number of bytes from s2 to concatenate
  *
  * Return: a pointer to allocated memory
  * **/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *newpointer;
+	void *newpointer;
 	unsigned int i;
 
-	if (nmemb == 0)
-		return (NULL);
-	if (size == 0)
-		return (NULL);
+	if (nmemb == 0 || size ==0)
+		return NULL;
 
 	newpointer = malloc((nmemb + 1) * size);
 
 	if (newpointer == NULL)
-		return (NULL);
+		return NULL;
 
-	for (i = 0; i < nmemb; i++)
-		newpointer[i] = '\0';
+	for(i = 0; i < nmemb; i ++)
+	{
+		if (size == sizeof(char))
+		{
+			*((char *)newpointer + i) = '\0';
+		}
+		if (size == sizeof(int))
+		{
+			*((int *)newpointer + i) = 0;
+		}
+	}
 
-	newpointer[i] = '\0';
 	return (newpointer);
 }

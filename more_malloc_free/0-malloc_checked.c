94d5049f8c94831b18315c0aee72639373f5631e
root <6677@holbertonstudents.com>
2023-06-25 19:07:13 -0700

    void ptr

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,20 @@
 #include "main.h"
 #include <stdlib.h>
-/** 
- * malloc_checked - function that allocates memory using maloc
+
+/**
+ * malloc_checked - function that allocates memory using malloc
  * @b: int of the status value
  *
- * Return: a pointer to the allocated memory.  If fails, status value is 98 
+ * Return: a pointer to the allocated memory.  If fails, status value is 98
  **/
 void *malloc_checked(unsigned int b)
 {
 	void *newpointer;
 	newpointer = malloc(b);
-
+	
 	if (newpointer == NULL)
 	{
 		exit(98);
 	}
-	return newpointer;
+	return (newpointer);
 }

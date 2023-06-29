8c6fd2a382a9ad35d627e3e2a7fc7bb33c5e8941
root <6677@holbertonstudents.com>
2023-06-29 13:08:18 -0700

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct my_dog
+ * @name: name of dog
+ * @age: age of dog
+ * @char: owner of dog
+ *
+ * Description: all about mydog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};

1ce67d8b8318d47702949de7ded16a2af36ed594
root <6677@holbertonstudents.com>
2023-07-02 16:03:03 -0700

    print

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -4,7 +4,7 @@
 
 /**
  * print_numbers -  a function that prints numbers followed by a new line
- * @separator - string to be printed to separate
+ * @separator: string to be printed to separate
  * @n: integers to be printed
  *
  * Return: void

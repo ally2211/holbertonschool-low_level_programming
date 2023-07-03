ecb7f100e58100fa72eb9d142cb013b89ab28d11
root <6677@holbertonstudents.com>
2023-07-02 17:21:29 -0700

    include

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -23,12 +23,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(ap,char *);
-		
+		str = va_arg(ap, char *);
+
 		if (i == 0)
 			printf("%s", str);
 		else if (str == NULL)
-                        printf("%s(nil)", separator);
+			printf("%s(nil)", separator);
 		else
 			printf("%s%s", separator, str);
 	}

7b48c2d1529139a50410bcdee52dea50c6415f64
root <6677@holbertonstudents.com>
2023-07-15 23:21:57 -0700

    print

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * print_binary - function prints binary representaion
+ * @n: number to be represented in binary
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask;
+	int leadingZeros = 1;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+
+	while (mask != 0)
+	{
+		if ((n & mask) != 0)
+		{
+			_putchar('1');
+			leadingZeros = 0;
+		}
+		else if (!leadingZeros)
+		{
+			_putchar('0');
+		}
+		mask >>= 1;
+	}
+}

1ba039da5ba074d0e65e34413e89462ee46c9de3
root <6677@holbertonstudents.com>
2023-07-18 19:07:02 -0700

    handle null

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -2,19 +2,20 @@
 #include "lists.h"
 
 /**
- * free_listint - frees a listint_t list.
+ * free_listint2 - frees a listint_t list.
  * @head: data structure head to null
  *
  * Return:  void
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
-	listint_t *next;
+	listint_t *current;
+        listint_t *next;
 
 	if (head == NULL)
-		return;
+                return;
 
+	current = *head;
 	while (current != NULL)
 	{
 		next = current->next;

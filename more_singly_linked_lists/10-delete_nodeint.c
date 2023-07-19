5ffc7f397cf4b510f63e8eb8c55b5b17c7882aa8
root <6677@holbertonstudents.com>
2023-07-18 21:01:02 -0700

    delete node

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -4,18 +4,18 @@
 /**
  * delete_nodeint_at_index - deletes a node at a given position.
  * @head: data structure head
- * @idx: index of the list where the new node should be deleted. starts at 0
+ * @index: index of the list where the new node should be deleted. starts at 0
  *
  * Return: 1 if succeeded and 0 if it failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-        listint_t *current;
-        listint_t *previous;
-        unsigned int count;
+	listint_t *current;
+	listint_t *previous;
+	unsigned int count;
 
 	if (head == NULL || *head == NULL)
-		return -1; 
+		return (-1);
 
 	current = *head;
 	previous = NULL;
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 	if (current == NULL)
-		return (-1); 
+		return (-1);
 
 	if (previous == NULL)
 	{

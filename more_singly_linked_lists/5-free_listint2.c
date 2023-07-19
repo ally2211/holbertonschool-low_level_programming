f8dac355c4cd4a4fcdf2abc2ffd2aea5ce6ec1a1
root <6677@holbertonstudents.com>
2023-07-18 19:11:56 -0700

    betty

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,10 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *current;
-        listint_t *next;
+	listint_t *next;
 
 	if (head == NULL)
-                return;
+		return;
 
 	current = *head;
 	while (current != NULL)

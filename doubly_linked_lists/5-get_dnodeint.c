13d22782dc28c455cdd2ef015fdb558edad60909
root <6677@holbertonstudents.com>
2023-07-21 14:26:57 -0700

    returns nth node

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint_end - adds a new node at the end of a doubly linkedblist.
+ * @head: a pointer to a pointer to the head of the dlistint_t list
+ * @index: the index of the node starting from 0.
+ *
+ * Return: address of the element or NULL if it failed
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current = head;
+	unsigned int count = 0;
+
+	while (current != NULL)
+	{
+		if (count == index)
+			return (current);
+
+		current = current->next;
+		count++;
+	}
+
+	return (NULL);
+}

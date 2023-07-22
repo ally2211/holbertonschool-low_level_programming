4eb695b86bf463319a7011c82a02f7439f3e9b7e
root <6677@holbertonstudents.com>
2023-07-21 21:18:03 -0700

    del

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -44,7 +44,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (current->next != NULL)
 		current->next->prev = current->prev;
 
-		free(current);
+	free(current);
 	return (1);
 }
 

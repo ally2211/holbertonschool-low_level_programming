d2accf55e4c726c3b56e1dd28808db8ee0ecd47c
root <6677@holbertonstudents.com>
2023-07-21 20:33:09 -0700

    insert

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include "lists.h"
+
 /**
- * insert_dnodeint_at_index - Inserts a new node at a given position.
+ * insert_dnodeint_at_index - Inserts a new node at a given position in a doubly linked list.
  * @h: A pointer to a pointer to the head of the dlistint_t list.
  * @idx: The index where the new node should be added. Index starts at 0.
  * @n: The value to store in the new node.
@@ -10,60 +11,67 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	/* Create a new node and allocate memory for it */
-	dlistint_t *new_node;
-	dlistint_t *current;
-	unsigned int count;
+    /* Create a new node and allocate memory for it */
+    dlistint_t *new_node;
+    dlistint_t *current;
+   unsigned int count;
 
-	if (h == NULL)
-		return (NULL);
-	
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	
-	/*set the value of the new node */
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
+    if (h == NULL)
+        return NULL;
 
-	/* If the list is empty and the index is 0, make the new node the head */
-	if (*h == NULL && idx == 0)
-	{
-		*h = new_node;
-		return (new_node);
-	}
+    new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL)
+        return NULL;
 
-	/* If the index is 0 but the list is not empty, insert the new node at the beginning */
-	if (idx == 0 && *h != NULL)
-	{
- 		new_node->next = *h;
-		(*h)->prev = new_node;
- 		*h = new_node;
- 		return new_node;
-	}
+    /* Set the value of the new node */
+    new_node->n = n;
+    new_node->prev = NULL;
+    new_node->next = NULL;
 
-	/* Traverse to the desired position in the list */
-	current = *h;
-	count = 0;
-	while (current != NULL && count < idx - 1)
-	{
-		current = current->next;
-		count++;
-	}
+    /* If the list is empty and the index is 0, make the new node the head */
+    if (*h == NULL && idx == 0)
+    {
+        *h = new_node;
+        return new_node;
+    }
 
-	/* If out of range or the previous node is NULL, return NULL */
-	if (current == NULL || (count == idx - 1 && current->next == NULL))
-	{
-		free(new_node);
-		return (NULL);
-	}
+    /* If the index is 0 but the list is not empty, insert the new node at the beginning */
+    if (idx == 0 && *h != NULL)
+    {
+        new_node->next = *h;
+        (*h)->prev = new_node;
+        *h = new_node;
+        return new_node;
+    }
 
-	/* Connect the new node between the current and the next nodes */
-	new_node->prev = current;
-	new_node->next = (count == idx - 1) ? NULL : current->next;
-	if (current->next != NULL)
-		current->next->prev = new_node;
-	current->next = new_node;
-	return (new_node);
+    /* Traverse to the desired position in the list */
+    current = *h;
+    count = 0;
+    
+    while (current != NULL && count < idx)
+    {
+        current = current->next;
+        count++;
+    }
+
+    /* If the current node is NULL and the desired index is greater than 0, or the previous node is NULL, return NULL */
+    if ((current == NULL && idx > 0) || (current == NULL && idx > count))
+    {
+        free(new_node);
+        return NULL;
+    }
+
+    /* Connect the new node between the current and the next nodes */
+    new_node->prev = current->prev;
+    new_node->next = current;
+
+    if (current->prev != NULL)
+        current->prev->next = new_node;
+    else
+        *h = new_node;
+
+    current->prev = new_node;
+
+    return new_node;
 }
+
